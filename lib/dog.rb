38b22371f495f85913858007d87ba0f13faf67f4
github username <github email address>
2020-03-08 22:12:27 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
 
-    attr_accessor :name, :breed, :id
+    attr_accessor :name, :breed, :id #has a name and a breed
 
-    def initialize(id: nil, name:, breed:)
+    def initialize(id: nil, name:, breed:) #has an id that defaults to `nil` on initialization and accepts key value pairs as arguments to initialize
         @id = id
         @name = name
         @breed = breed
     end
 
-    def self.create_table #
+    def self.create_table #creates the dogs table in the database
         sql = <<-SQL
             CREATE TABLE IF NOT EXISTS dogs (
                 id INTEGER PRIMARY KEY,
@@ -20,7 +20,7 @@ class Dog
         DB[:conn].execute(sql)
     end
 
-    def self.drop_table
+    def self.drop_table #drops the dogs table from the database
         sql = "DROP TABLE dogs"
 
         DB[:conn].execute(sql)
@@ -38,13 +38,13 @@ class Dog
         self #returns an instance of the dog class
     end
 
-    def self.create(name:, breed:)
+    def self.create(name:, breed:) #takes in a hash of attributes and uses metaprogramming to create a new dog object
         dog = Dog.new(name: name, breed: breed)
-        dog.save
-        dog
+        dog.save #then it uses the #save method to save that dog to the database
+        dog #returns a new dog object
     end
 
-    def self.new_from_db(row)
+    def self.new_from_db(row) #creates an instance with corresponding attribute values
         id = row[0]
         name = row[1]
         breed = row[2]
@@ -59,7 +59,7 @@ class Dog
             LIMIT 1
         SQL
 
-        DB[:conn].execute(sql, id).map do |row|
+        DB[:conn].execute(sql, id).map do |row| #returns a new dog object by id
             self.new_from_db(row)
         end.first
     end
@@ -75,13 +75,13 @@ class Dog
 
         dog = DB[:conn].execute(sql, name, breed)
 
-        if !dog.empty?
+        if !dog.empty? #creates an instance of a dog if it does not already exist
             dog_data = dog[0]
             dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-        else
+        else #when two dogs have the same name and different breed, it returns the correct dog
             dog = self.create(name: name, breed: breed)
         end
-        dog
+        dog #when creating a new dog with the same name as persisted dogs, it returns the correct dog
     end
 
     def self.find_by_name(name)
@@ -92,12 +92,12 @@ class Dog
             LIMIT 1
         SQL
 
-        DB[:conn].execute(sql, name).map do |row|
+        DB[:conn].execute(sql, name).map do |row| #returns an instance of dog that matches the name from the DB
             self.new_from_db(row)
         end.first
     end
 
-    def update
+    def update #updates the record associated with a given instance
         sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
 
         DB[:conn].execute(sql, self.name, self.breed, self.id)
